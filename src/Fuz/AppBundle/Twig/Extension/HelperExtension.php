9f8bb2e3c0e69a229d726302590486c7a4d8222c
alain tiemblo <ninsuo@gmail.com>
2017-02-09 22:03:31 +0100

    putting source code into tabs instead of giving github links

--- src/Fuz/AppBundle/Twig/Extension/HelperExtension.php
+++ src/Fuz/AppBundle/Twig/Extension/HelperExtension.php
@@ -15,10 +15,38 @@ class HelperExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('github', [$this, 'github'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('tabs', [$this, 'tabs'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('md5', 'md5', ['is_safe' => ['html']]),
         ];
     }
 
     public function github($files)
+    {
+        return $this->twig->render('FuzAppBundle::github.html.twig', [
+            'paths' => $this->_getPaths($files),
+        ]);
+    }
+
+    public function tabs($files)
+    {
+        $basedir  = realpath(__DIR__.'/../../');
+        $tabs = [];
+        foreach ($this->_getPaths($files) as $file) {
+            $tabs[$file] = file_get_contents($basedir.'/'.$file);
+        }
+
+        return $this->twig->render('FuzAppBundle::tabs.html.twig', [
+            'tabs' => $tabs,
+        ]);
+    }
+
+    protected function _getPaths($files)
     {
         $paths = [];
         foreach ($files as $file)
@@ -32,12 +60,9 @@ class HelperExtension extends \Twig_Extension
 
            $paths[] = substr($realpath, strlen($basedir) + 1);
         }
-
         sort($paths);
 
-        return $this->twig->render('FuzAppBundle::github.html.twig', [
-            'paths' => $paths
-        ]);
+        return $paths;
     }
 
     public function getName()
